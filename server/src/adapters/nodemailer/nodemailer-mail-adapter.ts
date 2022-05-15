ea09660b9d9f09d719656942b441662934183e9c
DKsHs <nicolas.contiero198@gmail.com>
2022-05-15 18:29:59 -0300

    finished backend

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, sendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "d52ddf4f0e5b6d",
+    pass: "70dc88f317dbc1",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: sendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Nicolas Contiero <nicolas.contiero198@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
